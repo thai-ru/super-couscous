fbb6e00bdd01113cdf69743cfacfe75c080a3f39
thai-ru <robinthairu34@gmail.com>
2024-04-12 09:48:59 +0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Robinson Thairu on 12/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
